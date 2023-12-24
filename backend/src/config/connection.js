e886a123de7c3f5ee01995037d1b59f716a087f4
zakka <mzakka163@gmail.com>
2023-12-24 12:34:58 +0700

    fix

--- backend/src/config/connection.js
+++ backend/src/config/connection.js
@@ -1,29 +1,29 @@
 const dotenv = require('dotenv').config({ path: './src/.env' })
 const { Sequelize } = require('sequelize');
 
-const sq = new Sequelize(process.env.PG) 
-// const sq = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: process.env.DB_DIAL,
-//   logging: false,
-//   dialectOptions: {
-//     dateStrings: true,
-//     typeCast: true,
-//   },
-//   ssl: {
-//     require: true,
-//     rejectUnauthorized: false,
-//     }
+// const sq = new Sequelize(process.env.PG) 
+const sq = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIAL,
+  logging: false,
+  dialectOptions: {
+    dateStrings: true,
+    typeCast: true,
+  },
+  // ssl: {
+  //   require: true,
+  //   rejectUnauthorized: false,
+  //   }
     
-//   // pool: {
-//   //   max: 100,
-//   //   min: 0,
-//   //   idle: 10000,
-//   //   acquire: 60000,
-//   // },
-//   // timezone: '+07:00'
-// });
+  // pool: {
+  //   max: 100,
+  //   min: 0,
+  //   idle: 10000,
+  //   acquire: 60000,
+  // },
+  // timezone: '+07:00'
+});
 
 async function tes() {
   try {
